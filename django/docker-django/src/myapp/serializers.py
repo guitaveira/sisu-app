ab29f35a8b150f664936c9e18c2d4364fffb276b
phdcoder <mestrealessandros@gmail.com>
2025-04-26 09:14:20 -0300

    1st commit

--- django/docker-django/src/myapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import  Feedback
-from rest_framework import  serializers
-
-# Serializers define the API representation.
-class FeedbackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Feedback
-        fields = '__all__'
--- php/app/vendor/mootensai/yii2-uuid-behavior/README.md
+++
@@ -1,5 +0,0 @@
-# yii2-uuid-behavior
-Set UUID on models
-
-Solution for this issue :
-https://github.com/yiisoft/yii2/issues/8390
