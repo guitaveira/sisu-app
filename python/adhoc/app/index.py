d52c3e5e2ef0a34fcdad6580aa1bd9ad5a888448
phdcoder <mestrealessandros@gmail.com>
2025-04-28 12:32:53 -0300

    Exercício de processamento de formulário !

--- python/adhoc/app/index.py
+++ python/adhoc/app/index.py
@@ -1,12 +1,19 @@
+import cgi
 from wsgiref import simple_server
 
 def app(environ, start_response):
-    data = b"Hello now Web!\n"
+    data=""
+    if environ['PATH_INFO']== "/app":
+        data = "Hello now Web!"
+    if environ['PATH_INFO']== "/app/feedback":
+        if environ['REQUEST_METHOD']== "POST":
+            form = cgi.FieldStorage(fp=environ["wsgi.input"],environ=environ)
+            data = form.getvalue('name') + " " + form.getvalue('email') + " " + form.getvalue('feedback')
     start_response ("200 OK",[
         ("Content-Type", "text/plain"),
         ("Content-Length",str(len(data)))
     ])
-    return [data]
+    return [data.encode('utf-8')]
 
 if __name__ == '__main__':
     w_s = simple_server.make_server(
