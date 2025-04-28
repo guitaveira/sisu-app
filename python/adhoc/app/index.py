8cd198bfaa0b6aaa5b7b654c865b59623bf66291
phdcoder <mestrealessandros@gmail.com>
2025-04-28 11:04:23 -0300

    Python versao imcompleta

---
+++ python/adhoc/app/index.py
@@ -0,0 +1,17 @@
+from wsgiref import simple_server
+
+def app(environ, start_response):
+    data = b"Hello Web!\n"
+    start_response ("200 OK",[
+        ("Content-Type", "text/plain"),
+        ("Content-Length",str(len(data)))
+    ])
+    return [data]
+
+if __name__ == '__main__':
+    w_s = simple_server.make_server(
+        host="",
+        port=8000,
+        app=app
+    )
+    w_s.serve_forever()
