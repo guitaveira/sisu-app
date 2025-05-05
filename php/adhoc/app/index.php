850460180de340264758e9d89dc83b0af5ebc020
phdcoder <mestrealessandros@gmail.com>
2025-05-05 09:46:11 -0300

    versao php  form feedback

---
+++ php/adhoc/app/index.php
@@ -0,0 +1,26 @@
+<?php
+
+function app() {
+    $path = $_SERVER["REQUEST_URI"];
+    $method = $_SERVER["REQUEST_METHOD"];
+    $data = "";
+    if ($path == "/app") {
+        $data = "Hello World on php!";
+    } else {
+        if ($path == "/app/feedback") {
+            if ($method == "POST") {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $feedback = $_POST['feedback'];
+                $data = "$name $email $feedback";
+            }
+            else {
+                include ('feedback.php');
+            }
+        }
+    }
+
+    echo $data;
+}
+
+app();
